14db5b754292ffb5e2b5112a9f1b5e3cedc399c6
Alex Dvornikov <a.dvornikov2102@gmail.com>
2025-08-08 17:35:24 +0300

    adjust working files and folders, moved all logic to main.go in root

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import "fmt"
 
-//Lesson 4 Homework
+// Lesson 4 Homework
+
+// func main() {
+// 	fmt.Println("Hello world!")
+// }
+
+// Lesson 5 Homework
+var (
+	first_number  int
+	second_number int
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println("Please enter the first number: ")
+	fmt.Scan(&first_number)
+	fmt.Println("Please enter the second number: ")
+	fmt.Scan(&second_number)
+	fmt.Println(first_number, second_number)
 }
