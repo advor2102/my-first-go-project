c5b1be059a57e3a38247df56ffbce07491d333de
Alex Dvornikov <a.dvornikov2102@gmail.com>
2025-08-14 14:26:02 +0300

    chore: comment Lesson 5 Homework and creat base for Lesson 6 Homework

--- main.go
+++ main.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
 // Lesson 4 Homework
 
 // func main() {
@@ -13,67 +8,70 @@ import (
 
 // Lesson 5 Homework
 
-var (
-	dividend   float64
-	devisor    float64
-	quotient   float64
-	userAnswer string
-)
+// var (
+// 	dividend   float64
+// 	devisor    float64
+// 	quotient   float64
+// 	userAnswer string
+// )
 
-func division(dividend, devisor float64) (float64, error) {
-	if devisor == 0 {
-		return 0, fmt.Errorf("error: you can't divide by zero")
-	}
-	quotient = dividend / devisor
-	return quotient, nil
-}
+// func division(dividend, devisor float64) (float64, error) {
+// 	if devisor == 0 {
+// 		return 0, fmt.Errorf("error: you can't divide by zero")
+// 	}
+// 	quotient = dividend / devisor
+// 	return quotient, nil
+// }
 
+// func main() {
+// 	for {
+// 		fmt.Println("Please enter the first number: ")
+// 		_, err := fmt.Scan(&dividend)
+// 		if err != nil {
+// 			fmt.Println("error: you should enter number")
+// 			fmt.Println("Do you want to use another numbers?(y/n)")
+// 			fmt.Scan(&userAnswer)
+// 			if strings.ToLower(userAnswer) == "y" {
+// 				continue
+// 			} else if strings.ToLower(userAnswer) == "n" {
+// 				break
+// 			}
+// 		}
+// 		fmt.Println("Please enter the second number: ")
+// 		_, err = fmt.Scan(&devisor)
+// 		if err != nil {
+// 			fmt.Println("error: you should enter number")
+// 			fmt.Println("Do you want to use another numbers?(y/n)")
+// 			fmt.Scan(&userAnswer)
+// 			if strings.ToLower(userAnswer) == "y" {
+// 				continue
+// 			} else if strings.ToLower(userAnswer) == "n" {
+// 				break
+// 			}
+// 		}
+// 		divisionResult, zeroDivisionError := division(dividend, devisor)
+// 		if zeroDivisionError != nil {
+// 			fmt.Println(zeroDivisionError)
+// 		} else {
+// 			fmt.Println(divisionResult)
+// 			switch {
+// 			case divisionResult > 10:
+// 				fmt.Println("Результат большой")
+// 			case divisionResult == 1 && divisionResult <= 10:
+// 				fmt.Println("Результат средний")
+// 			default:
+// 				fmt.Println("Результат маленький или ноль")
+// 			}
+// 		}
+// 		fmt.Println("Do you want to use another numbers?(y/n)")
+// 		fmt.Scan(&userAnswer)
+// 		if strings.ToLower(userAnswer) == "y" {
+// 			continue
+// 		} else if strings.ToLower(userAnswer) == "n" {
+// 			break
+// 		}
+// 	}
+// }
 func main() {
-	for {
-		fmt.Println("Please enter the first number: ")
-		_, err := fmt.Scan(&dividend)
-		if err != nil {
-			fmt.Println("error: you should enter number")
-			fmt.Println("Do you want to use another numbers?(y/n)")
-			fmt.Scan(&userAnswer)
-			if strings.ToLower(userAnswer) == "y" {
-				continue
-			} else if strings.ToLower(userAnswer) == "n" {
-				break
-			}
-		}
-		fmt.Println("Please enter the second number: ")
-		_, err = fmt.Scan(&devisor)
-		if err != nil {
-			fmt.Println("error: you should enter number")
-			fmt.Println("Do you want to use another numbers?(y/n)")
-			fmt.Scan(&userAnswer)
-			if strings.ToLower(userAnswer) == "y" {
-				continue
-			} else if strings.ToLower(userAnswer) == "n" {
-				break
-			}
-		}
-		divisionResult, zeroDivisionError := division(dividend, devisor)
-		if zeroDivisionError != nil {
-			fmt.Println(zeroDivisionError)
-		} else {
-			fmt.Println(divisionResult)
-			switch {
-			case divisionResult > 10:
-				fmt.Println("Результат большой")
-			case divisionResult == 1 && divisionResult <= 10:
-				fmt.Println("Результат средний")
-			default:
-				fmt.Println("Результат маленький или ноль")
-			}
-		}
-		fmt.Println("Do you want to use another numbers?(y/n)")
-		fmt.Scan(&userAnswer)
-		if strings.ToLower(userAnswer) == "y" {
-			continue
-		} else if strings.ToLower(userAnswer) == "n" {
-			break
-		}
-	}
+
 }
