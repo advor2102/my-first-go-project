6ba087723286e3fb0a8b8def252b649cecbd2c73
Alex Dvornikov <a.dvornikov2102@gmail.com>
2025-08-15 10:34:37 +0300

    feat: declared new map

--- main.go
+++ main.go
@@ -79,6 +79,7 @@ import "fmt"
 
 var arr = [5]int{1, 2, 3, 4, 5}
 var slice = []int{1, 2, 3, 4, 5}
+var ages = map[string]int{"Ann": 30, "John": 45, "Marie": 28}
 
 func sum(fTerm int, sTerm int) int {
 	sum := fTerm + sTerm
@@ -113,4 +114,5 @@ func main() {
 	fmt.Println(slice)
 	fmt.Println(newSlice)
 	fmt.Print(unite(slice, newSlice))
+	fmt.Println(ages)
 }
