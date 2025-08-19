aa587cc814963359c6ce5c77317da94b9a295e60
Alex Dvornikov <a.dvornikov2102@gmail.com>
2025-08-19 11:33:14 +0300

    feat: added odd and even function and two new goruitines and used in functions

---
+++ lesson8/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func oddN(nums []int, wg *sync.WaitGroup) {
+	for _, v := range nums {
+		if v%2 == 0 {
+			fmt.Println(v)
+		}
+	}
+	wg.Done()
+}
+
+func evenN(nums []int, wg *sync.WaitGroup) {
+	for _, v := range nums {
+		if v%2 == 1 {
+			fmt.Println(v)
+		}
+	}
+	wg.Done()
+}
+
+func main() {
+	var wg sync.WaitGroup
+	arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	wg.Add(1)
+	go oddN(arr, &wg)
+
+	wg.Add(1)
+	go evenN(arr, &wg)
+
+	wg.Wait()
+}
