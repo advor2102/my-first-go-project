4b7d0dce1316f36512078bbea6b402b1dccf0035
Alex Dvornikov <a.dvornikov2102@gmail.com>
2025-08-19 13:22:45 +0300

    feat: added functions for download 3 files

--- lesson8/main.go
+++ lesson8/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 // 1. Печать чисел в двух горутинах
@@ -25,6 +26,26 @@ func evenN(nums []int, wg *sync.WaitGroup) {
 	wg.Done()
 }
 
+//2. Параллельная загрузка
+
+func dFirstFile(wg *sync.WaitGroup) {
+	time.Sleep(1 * time.Second)
+	fmt.Println("file1 loaded")
+	wg.Done()
+}
+
+func dSecondFile(wg *sync.WaitGroup) {
+	time.Sleep(2 * time.Second)
+	fmt.Println("file2 loaded")
+	wg.Done()
+}
+
+func dFirdFile(wg *sync.WaitGroup) {
+	time.Sleep(500 * time.Millisecond)
+	fmt.Println("file3 loaded")
+	wg.Done()
+}
+
 func main() {
 	var wg sync.WaitGroup
 	arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
@@ -34,6 +55,16 @@ func main() {
 
 	wg.Add(1)
 	go evenN(arr, &wg)
+	wg.Wait()
 
+	wg.Add(1)
+	dFirstFile(&wg)
+
+	wg.Add(1)
+	dSecondFile(&wg)
+	wg.Wait()
+
+	wg.Add(1)
+	dFirdFile(&wg)
 	wg.Wait()
 }
