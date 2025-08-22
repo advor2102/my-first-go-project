b593769261141da50f03df874f366f72e9025b02
Alex Dvornikov <a.dvornikov2102@gmail.com>
2025-08-22 17:22:19 +0300

    feat: added 2 functions to send to chanel and read from it

--- lesson9/main.go
+++ lesson9/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main()
+import (
+	"fmt"
+)
+
+func addValToChan(ch chan int) {
+	for i := 0; i <= 20; i++ {
+		ch <- i
+	}
+}
+
+func readValFromChan(ch chan int) {
+	for num := range ch {
+		fmt.Println(num)
+	}
+}
+func main() {
+	ch := make(chan int, 5)
+	go addValToChan(ch)
+	readValFromChan(ch)
+}
