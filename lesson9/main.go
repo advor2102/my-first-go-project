a24894944697d12df3d34826c04bcc9de7aa0883
Alex Dvornikov <a.dvornikov2102@gmail.com>
2025-08-24 20:09:21 +0300

    feat: added sample of listening two chanels with select case

--- lesson9/main.go
+++ lesson9/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func addValToChan(ch chan int) {
@@ -18,8 +19,47 @@ func readValFromChan(ch chan int) {
 
 	}
 }
+
+func sendToFirstCahnel(ch1 chan string) {
+	defer close(ch1)
+	for i := 1; i <= 10; i++ {
+		ch1 <- fmt.Sprintf("ch1: %d", i)
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+func sendToSecCahnel(ch2 chan string) {
+	defer close(ch2)
+	for i := 1; i <= 20; i++ {
+		ch2 <- fmt.Sprintf("ch2: %d", i)
+		time.Sleep(300 * time.Millisecond)
+	}
+}
+
 func main() {
 	ch := make(chan int, 5)
+	ch1 := make(chan string)
+	ch2 := make(chan string)
 	go addValToChan(ch)
 	readValFromChan(ch)
+	go sendToFirstCahnel(ch1)
+	go sendToSecCahnel(ch2)
+	for ch1 != nil || ch2 != nil {
+		select {
+		case massage, ok := <-ch1:
+			if !ok {
+				ch1 = nil
+				fmt.Println("ch1 closed")
+				continue
+			}
+			fmt.Println("Received:", massage)
+		case massage, ok := <-ch2:
+			if !ok {
+				ch2 = nil
+				fmt.Println("ch2 closed")
+				continue
+			}
+			fmt.Println("Received:", massage)
+		}
+	}
 }
