2ad5dbe47b6f345b0a210964a29419af00d55be2
Alex Dvornikov <a.dvornikov2102@gmail.com>
2025-08-25 19:23:48 +0300

    feat: added load datat function to simulate data loading

--- lesson9/main.go
+++ lesson9/main.go
@@ -36,48 +36,63 @@ func sendToSecCahnel(ch2 chan string) {
 	}
 }
 
+func loadData(resultCh chan<- string) {
+	fmt.Println("Loading data...")
+	time.Sleep(2 * time.Second)
+	resultCh <- "Data loaded successful"
+}
+
 func main() {
-	ch := make(chan int, 5)
-	ch1 := make(chan string)
-	ch2 := make(chan string)
-	go addValToChan(ch)
-	readValFromChan(ch)
-	go sendToFirstCahnel(ch1)
-	go sendToSecCahnel(ch2)
-	timer := time.NewTimer(1500 * time.Millisecond)
-	for ch1 != nil || ch2 != nil {
-		select {
-		case massage, ok := <-ch1:
-			if !ok {
-				ch1 = nil
-				fmt.Println("ch1 closed")
-				continue
-			}
-			fmt.Println("Received:", massage)
-			if !timer.Stop() {
-				select {
-				case <-timer.C:
-				default:
-				}
-			}
-			timer.Reset(1500 * time.Millisecond)
-		case massage, ok := <-ch2:
-			if !ok {
-				ch2 = nil
-				fmt.Println("ch2 closed")
-				continue
-			}
-			fmt.Println("Received:", massage)
-			if !timer.Stop() {
-				select {
-				case <-timer.C:
-				default:
-				}
-			}
-			timer.Reset(1500 * time.Millisecond)
-		case <-timer.C:
-			fmt.Println("Timeout: no data received for 1.5 seconds, exiting")
-			return
-		}
+	// ch := make(chan int, 5)
+	// ch1 := make(chan string)
+	// ch2 := make(chan string)
+	resultCh := make(chan string)
+	// go addValToChan(ch)
+	// readValFromChan(ch)
+	// go sendToFirstCahnel(ch1)
+	// go sendToSecCahnel(ch2)
+	// timer := time.NewTimer(1500 * time.Millisecond)
+	// for ch1 != nil || ch2 != nil {
+	// 	select {
+	// 	case massage, ok := <-ch1:
+	// 		if !ok {
+	// 			ch1 = nil
+	// 			fmt.Println("ch1 closed")
+	// 			continue
+	// 		}
+	// 		fmt.Println("Received:", massage)
+	// 		if !timer.Stop() {
+	// 			select {
+	// 			case <-timer.C:
+	// 			default:
+	// 			}
+	// 		}
+	// 		timer.Reset(1500 * time.Millisecond)
+	// 	case massage, ok := <-ch2:
+	// 		if !ok {
+	// 			ch2 = nil
+	// 			fmt.Println("ch2 closed")
+	// 			continue
+	// 		}
+	// 		fmt.Println("Received:", massage)
+	// 		if !timer.Stop() {
+	// 			select {
+	// 			case <-timer.C:
+	// 			default:
+	// 			}
+	// 		}
+	// 		timer.Reset(1500 * time.Millisecond)
+	// 	case <-timer.C:
+	// 		fmt.Println("Timeout: no data received for 1.5 seconds")
+	// 		return
+	// 	}
+	// }
+	go loadData(resultCh)
+	fmt.Println("Waiting...")
+	select {
+	case result := <-resultCh:
+		fmt.Println("Result:", result)
+	case <-time.After(3 * time.Second):
+		fmt.Println("Timout: no data received for 3 seconds")
 	}
 }
