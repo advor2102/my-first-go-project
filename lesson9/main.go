0518edb07794c1ef28fb839cc4fcb5d9b112b660
Alex Dvornikov <a.dvornikov2102@gmail.com>
2025-08-26 17:23:26 +0300

    feat: added sample with 3 workers

--- lesson9/main.go
+++ lesson9/main.go
@@ -91,6 +91,14 @@ func wContextCancel(ctx context.Context) {
 	}
 }
 
+func worker(id int, jobs <-chan int, results chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for num := range jobs {
+		fmt.Printf("Worker %d processing job %d\n", id, num)
+		results <- num * 2
+	}
+}
+
 func main() {
 	// ch := make(chan int, 5)
 	// ch1 := make(chan string)
@@ -153,11 +161,30 @@ func main() {
 	// } else {
 	// 	fmt.Println("Success")
 	// }
-	ctx, cancel := context.WithCancel(context.Background())
-	go wContextCancel(ctx)
-	time.Sleep(2 * time.Second)
-	fmt.Println("Cancel func")
-	cancel()
-	time.Sleep(1 * time.Second)
-	fmt.Println("Finish")
+	// ctx, cancel := context.WithCancel(context.Background())
+	// go wContextCancel(ctx)
+	// time.Sleep(2 * time.Second)
+	// fmt.Println("Cancel func")
+	// cancel()
+	// time.Sleep(1 * time.Second)
+	// fmt.Println("Finish")
+	jobs := make(chan int, 3)
+	results := make(chan int, 3)
+	var wg sync.WaitGroup
+	for w := 1; w <= 3; w++ {
+		wg.Add(1)
+		go worker(w, jobs, results, &wg)
+	}
+	for j := 1; j <= 3; j++ {
+		jobs <- j
+	}
+	close(jobs)
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+	for res := range results {
+		fmt.Println("Result:", res)
+	}
+	fmt.Println("Finished")
 }
