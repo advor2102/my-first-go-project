8ef5debefc7834c335c7b4002031330061d02258
Alex Dvornikov <a.dvornikov2102@gmail.com>
2025-08-27 18:15:34 +0300

    feat: added function to read json congig file and added to slice, increasin age on 1

--- lesson10/main.go
+++ lesson10/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func loadConfigJSON(path string) ([]User, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var cfg []User
+	if err = json.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
+
+func main() {
+	file := `C:\Users\AlexanderDvornikov\golang_course\my-first-go-project\lesson10\config.json`
+	data, err := loadConfigJSON(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("First version: %+v\n", data)
+	for i, _ := range data {
+		data[i].Age += 1
+	}
+	fmt.Printf("Version after age increasing: %+v\n", data)
 }
