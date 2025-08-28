adeab04c90b808d79595db540608e86f42bc340a
Alex Dvornikov <a.dvornikov2102@gmail.com>
2025-08-28 16:44:40 +0300

    feat: added function to read xml file, umarshall it and  increasin year for each book and marshall it

--- lesson10/main.go
+++ lesson10/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"os"
 )
@@ -9,6 +10,14 @@ import (
 type User struct {
 	Name string `json:"name"`
 	Age  int    `json:"age"`
+}
+
+type Book struct {
+	Title string `xml:"title"`
+	Year  int    `xml:"year"`
+}
+type Library struct {
+	Books []Book `xml:"book"`
 }
 
 func loadConfigJSON(path string) ([]User, error) {
@@ -23,15 +32,48 @@ func loadConfigJSON(path string) ([]User, error) {
 	return cfg, nil
 }
 
+func loadConfigXML(path string) (Library, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return Library{}, err
+	}
+	var b Library
+	err = xml.Unmarshal(data, &b)
+	return b, err
+}
+
 func main() {
 	fileJSON := `C:\Users\AlexanderDvornikov\golang_course\my-first-go-project\lesson10\users.json`
 	data, err := loadConfigJSON(fileJSON)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Printf("First version: %+v\n", data)
+	fmt.Printf("First version JSON: %+v\n", data)
 	for i, _ := range data {
 		data[i].Age += 1
 	}
-	fmt.Printf("Version after age increasing: %+v\n", data)
+	fmt.Printf("Version after age increasing JSON: %+v\n", data)
+	raw, err := json.MarshalIndent(data, "", " ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(raw))
+
+	fileXML := `C:\Users\AlexanderDvornikov\golang_course\my-first-go-project\lesson10\books.xml`
+	library, err := loadConfigXML(fileXML)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("First version XML: %+v\n", library)
+	for i := range library.Books {
+		library.Books[i].Year++
+	}
+	fmt.Printf("Version after age increasing XML: %+v\n", library)
+	rawXML, err := xml.MarshalIndent(library, "", " ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(rawXML))
 }
