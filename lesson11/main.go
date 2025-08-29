0897db9c6ac34ea501bb6a7697208e70f8722db9
Alex Dvornikov <a.dvornikov2102@gmail.com>
2025-08-29 20:52:12 +0300

    feat: added net/http echo-server

---
+++ lesson11/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func helloBody(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Reading body error", http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(w, "Got: %s", body)
+}
+
+func main() {
+	http.HandleFunc("/echo", helloBody)
+	fmt.Println("Started listening port: 8081")
+	http.ListenAndServe(":8081", nil)
+}
