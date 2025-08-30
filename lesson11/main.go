c9b954685d6b8a910092c7076358d38d4807de0c
Alex Dvornikov <a.dvornikov2102@gmail.com>
2025-08-30 19:20:47 +0300

    feat: added new greet endpoint and handler for it

--- lesson11/main.go
+++ lesson11/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
 
+type User struct {
+	Name string `json:"name"`
+}
+
+type UserResponse struct {
+	Massage string `json:"massage"`
+}
+
 func main() {
 	http.HandleFunc("/echo", helloBody)
+	http.HandleFunc("/greet", greet)
 	fmt.Println("Started listening port: 8081")
 	http.ListenAndServe(":8081", nil)
 }
@@ -19,4 +29,20 @@ func helloBody(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Fprintf(w, "Got: %s", body)
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	var (
+		user User
+		resp UserResponse
+	)
+
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Reading body error", http.StatusBadRequest)
+		return
+	}
+	resp = UserResponse{Massage: "Hello," + user.Name}
+	json.NewEncoder(w).Encode(resp)
 }
